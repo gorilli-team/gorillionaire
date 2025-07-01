79afc72f7f946494bc8245da90342cb5afca5254
Marco Roccon <marco.roccon@gmail.com>
2025-07-01 12:09:56 +0200

    test: increase test coverage for SmartWalletFactory contract

--- contracts/smart-wallet/src/SmartWalletFactory.sol
+++ contracts/smart-wallet/src/SmartWalletFactory.sol
@@ -6,19 +6,19 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SmartWalletFactory is Ownable {
     event SmartWalletCreated(address indexed user, address indexed smartWallet);
-    event MaxWalletPerUserUpdated(uint256 indexed newLimit);
+    event UserWalletLimitUpdated(uint256 indexed newLimit);
     
     error SmartWalletFactory__ExceededWalletLimit();
 
     SmartWallet[] public s_deployedSmartWallets;
     mapping(address user => SmartWallet[] smartWallets) public s_userToSmartWallets;
 
-    uint256 public s_maxWalletPerUser = 1;
+    uint256 public s_userWalletLimit = 1;
 
     constructor() Ownable(msg.sender) {}
 
-    function createSmartWallet() public {
-        if(s_userToSmartWallets[msg.sender].length >= s_maxWalletPerUser) {
+    function createSmartWallet() public returns(SmartWallet) {
+        if(s_userToSmartWallets[msg.sender].length >= s_userWalletLimit) {
             revert SmartWalletFactory__ExceededWalletLimit();
         }
 
@@ -27,10 +27,16 @@ contract SmartWalletFactory is Ownable {
         s_userToSmartWallets[msg.sender].push(smartWallet);
 
         emit SmartWalletCreated(msg.sender, address(smartWallet));
+
+        return smartWallet;
     }
 
-    function setMaxWalletPerUser(uint256 _newLimit) public onlyOwner {
-        s_maxWalletPerUser = _newLimit;
-        emit MaxWalletPerUserUpdated(_newLimit);
+    function setUserWalletLimit(uint256 _newLimit) public onlyOwner {
+        s_userWalletLimit = _newLimit;
+        emit UserWalletLimitUpdated(_newLimit);
+    }
+
+    function getUserSmartWallets(address _user) public view returns(SmartWallet[] memory) {
+        return s_userToSmartWallets[_user];
     }
 }
--- contracts/smart-wallet/test/SmartWalletFactoryTest.t.sol
+++ contracts/smart-wallet/test/SmartWalletFactoryTest.t.sol
@@ -1,12 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Test } from "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
 import { SmartWallet } from "../src/SmartWallet.sol";
 import { SmartWalletFactory } from "../src/SmartWalletFactory.sol";
 
 contract SmartWalletFactoryTest is Test {
+    SmartWalletFactory smartWalletFactory;
+
+    address deployer = makeAddr("deployer");
+    address user1 = makeAddr("user1");
+
     function setUp() public {
+        vm.prank(deployer);
+        smartWalletFactory = new SmartWalletFactory();
+    }
+
+    function test_createSmartWallet() public {
+        vm.prank(deployer);
+        SmartWallet smartWallet = smartWalletFactory.createSmartWallet();
+
+        address smartWalletOwner = smartWallet.s_owner();
+
+        assertEq(smartWalletOwner, deployer);
+    }
+
+    function test_createSmartWalletShouldFailIfExceededWalletLimit() public {
+        vm.prank(deployer);
+        smartWalletFactory.createSmartWallet();
+
+        vm.prank(deployer);
+        vm.expectRevert(SmartWalletFactory.SmartWalletFactory__ExceededWalletLimit.selector);
+        smartWalletFactory.createSmartWallet();
+    }
+
+    function test_setUserWalletLimit() public {
+        vm.prank(deployer);
+        smartWalletFactory.setUserWalletLimit(2);
+
+        uint256 currentUserWalletLimit = smartWalletFactory.s_userWalletLimit();
+
+        assertEq(currentUserWalletLimit, 2);
+    }
+
+    function test_userShouldBeAbleToCreateMoreWalletsAfterUpdatingUserWalletLimit() public {
+        vm.prank(user1);
+        smartWalletFactory.createSmartWallet();
+
+        vm.prank(user1);
+        vm.expectRevert(SmartWalletFactory.SmartWalletFactory__ExceededWalletLimit.selector);
+        smartWalletFactory.createSmartWallet();
         
+        vm.prank(deployer);
+        smartWalletFactory.setUserWalletLimit(2);
+
+        vm.prank(user1);
+        smartWalletFactory.createSmartWallet();
+
+        uint256 numberOfSmartWalletsUser1 = smartWalletFactory.getUserSmartWallets(user1).length;
+
+        assertEq(numberOfSmartWalletsUser1, 2);
     }
 }
