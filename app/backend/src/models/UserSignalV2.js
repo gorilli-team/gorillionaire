4778acd62e5f226856a40c15ffec6ff79c666e3f
SamuelePassiatore <samuw.978@gmail.com>
2025-07-15 17:03:39 +0200

    create user signal v2 model

---
+++ app/backend/src/models/UserSignalV2.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UserSignalV2Schema = new mongoose.Schema({
+  userAddress: {
+    type: String,
+    required: true,
+    index: true
+  },
+  signalId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  choice: {
+    type: String,
+    enum: ["Yes", "No"],
+    required: true
+  },
+  tokenSymbol: {
+    type: String,
+    required: true
+  },
+  actionType: {
+    type: String,
+    enum: ["Buy", "Sell"],
+    required: true
+  },
+  priceAtSignal: {
+    type: Number,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+    index: true
+  }
+});
+
+UserSignalV2Schema.index({ userAddress: 1, signalId: 1 }, { unique: true });
+
+module.exports = mongoose.model("User-Signal-V2", UserSignalV2Schema);
