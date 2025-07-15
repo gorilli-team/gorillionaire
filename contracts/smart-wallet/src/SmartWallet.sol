b51c6d6fcc3c57e78a06ba0f6606c1f046bd944a
Marco Roccon <marco.roccon@gmail.com>
2025-07-15 16:38:46 +0200

    contracts: add natspecs for SmartWallet and SmartWalletFactory functions

--- contracts/smart-wallet/src/SmartWallet.sol
+++ contracts/smart-wallet/src/SmartWallet.sol
@@ -5,6 +5,13 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IUniswapV2Router02} from "./interfaces/IUniswapV2Router02.sol";
 import {IUniswapV2Factory} from "./interfaces/IUniswapV2Factory.sol";
 
+/**
+ * @title A Smart Wallet for trading with the Gorillionaire bot automation
+ * @author Gorilli
+ * @notice This contract represents a Smart Wallet that any user can deploy via the SmartWalletFactory
+ * The Smart Wallet manages user funds and restricts access to only the Smart Wallet owner and the bot operator,
+ * each with permission to perform specific actions.
+ */
 contract SmartWallet {
     event AuthorizeOperator(address indexed operator, bool indexed authorized);
     event SetWhitelistedRouter(address indexed router, bool indexed whitelisted);
@@ -83,6 +90,12 @@ contract SmartWallet {
         _;
     }
 
+    /**
+     * @notice Deposits a specified amount of USDC into the Smart Wallet
+     * @param amount The amount of USDC to deposit
+     * @dev Only the Smart Wallet owner can call this function
+     * @dev The specified amount must be approved to this contract before calling
+     */
     function depositUSDC(uint256 amount) public onlyOwner {
         if (amount <= 0) {
             revert SmartWallet__InvalidAmount();
@@ -97,6 +110,13 @@ contract SmartWallet {
         emit DepositUSDC(address(usdc), amount);
     }
 
+    /**
+     * @notice Withdraws a specified amount of USDC from the Smart Wallet
+     * @param amount The amount of USDC to withdraw
+     * @dev Only the Smart Wallet owner can call this function
+     * @dev The function reverts if the Smart Wallet has no USDC balance
+     * @dev The specified amount must not exceed the current USDC balance of the Smart Wallet
+     */
     function withdrawUSDC(uint256 amount) public onlyOwner {
         if (amount <= 0) {
             revert SmartWallet__InvalidAmount();
@@ -121,6 +141,11 @@ contract SmartWallet {
         emit WithdrawUSDC(address(usdc), amount);
     }
 
+    /**
+     * @notice Withdraws all the USDC in the Smart Wallet
+     * @dev Only the Smart Wallet owner can call this function
+     * @dev The function reverts if the Smart Wallet has no USDC balance
+     */
     function withdrawAllUSDC() public onlyOwner {
         IERC20 usdc = IERC20(i_usdc);
         uint256 usdcBalance = usdc.balanceOf(address(this));
@@ -137,6 +162,16 @@ contract SmartWallet {
         emit WithdrawUSDC(address(usdc), usdcBalance);
     }
 
+    /**
+     * @notice Buys a given amount of tokenOut
+     * @param router The address of the UniswapV2Router02 contract
+     * @param tokenOut The address of the token to be received
+     * @param amountOut The exact amount of tokenOut to receive
+     * @param amountInMax The maximum amount of tokenIn to send
+     * @dev The router address must have been approved by the owner of the Smart Wallet
+     * @dev Only the allowed operator(s) can call this function
+     * @dev tokenIn is always USDC
+     */
     function buyTokens(address router, address tokenOut, uint256 amountOut, uint256 amountInMax) external onlyOperator onlyValidSwapParameters(router, tokenOut, amountOut) {
         uint256 currentAllowance = IERC20(i_usdc).allowance(address(this), router);
         if(currentAllowance < amountInMax) {
@@ -160,6 +195,16 @@ contract SmartWallet {
         emit BuyTokens(address(this), pair, i_usdc, tokenOut, msg.sender, amounts[0], amountOut, amountInMax);
     }
 
+    /**
+     * @notice Sells a given amount of tokenIn
+     * @param router The address of the UniswapV2Router02 contract
+     * @param tokenIn The address of the token to be sent
+     * @param amountIn The exact amount of tokenIn to send
+     * @param amountOutMin The minimum amount of tokenOut to receive
+     * @dev The router address must have been approved by the owner of the Smart Wallet
+     * @dev Only the allowed operator(s) can call this function
+     * @dev tokenOut is always USDC
+     */
     function sellTokens(address router, address tokenIn, uint256 amountIn, uint256 amountOutMin) external onlyOperator onlyValidSwapParameters(router, tokenIn, amountIn) {
         uint256 currentAllowance = IERC20(tokenIn).allowance(address(this), router);
         if(currentAllowance < amountIn) {
@@ -184,21 +229,35 @@ contract SmartWallet {
         emit SellTokens(address(this), pair, tokenIn, i_usdc, msg.sender, amountIn, amounts[amounts.length - 1], amountOutMin);
     }
 
-    function setOperator(address _operator, bool authorized) external onlyOwner {
-        if(_operator == address(0)) {
+    /**
+     * @notice Sets the authorization status of an operator
+     * @param operator The address of the operator (e.g., the Gorillionaire bot)
+     * @param authorized True to authorize the operator, false to revoke authorization
+     * @dev Only the owner of the Smart Wallet can call this function
+     * @dev To revoke authorization, set `authorized` to false
+     */
+    function setOperator(address operator, bool authorized) external onlyOwner {
+        if(operator == address(0)) {
             revert SmartWallet__InvalidOperatorAddress();
         }
-        s_isOperator[_operator] = authorized;
+        s_isOperator[operator] = authorized;
 
-        emit AuthorizeOperator(_operator, authorized);
+        emit AuthorizeOperator(operator, authorized);
     }
 
-    function setWhitelistedRouter(address _router, bool whitelisted) external onlyOwner {
-        if(_router == address(0)) {
+    /**
+     * @notice Sets the whitelisted status of a router
+     * @param router The address of the UniswapV2Router02 contract
+     * @param whitelisted True to whitelist the router, false to remove it from the whitelist
+     * @dev Only the owner of the Smart Wallet can call this function
+     * @dev To remove a router from the whitelist, set `whitelisted` to false
+     */
+    function setWhitelistedRouter(address router, bool whitelisted) external onlyOwner {
+        if(router == address(0)) {
             revert SmartWallet__InvalidRouterAddress();
         }
-        s_isWhitelistedRouter[_router] = whitelisted;
+        s_isWhitelistedRouter[router] = whitelisted;
 
-        emit SetWhitelistedRouter(_router, whitelisted);
+        emit SetWhitelistedRouter(router, whitelisted);
     }
 }
